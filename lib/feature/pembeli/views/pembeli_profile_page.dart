64671b7f1579a5f40527973711f7cef33ed82849
M.Tegar P H <47026803+mtegarph@users.noreply.github.com>
2022-12-21 20:49:18 +0700

    add ganti kawasan di profile page

--- lib/feature/pembeli/views/pembeli_profile_page.dart
+++ lib/feature/pembeli/views/pembeli_profile_page.dart
@@ -3,6 +3,7 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:cafetaria/feature/Authentication/authentication.dart';
 import 'package:cafetaria/feature/Authentication/bloc/authentication/authentication_bloc.dart';
+import 'package:cafetaria/feature/Authentication/views/pilih_kawasan.dart';
 import 'package:cafetaria/styles/colors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -119,142 +120,192 @@ class _PembeliProfileState extends State<PembeliProfileView> {
           body: SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+              child: SingleChildScrollView(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CircleAvatar(
-                        radius: 28, // Image radius,
-                        backgroundColor: MyColors.grey3,
-                        backgroundImage: NetworkImage(user.photoURL!),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 28, // Image radius,
+                            backgroundColor: MyColors.grey3,
+                            backgroundImage: NetworkImage(user.photoURL!),
+                          ),
+                          const SizedBox(width: 16),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "${user.displayName}",
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Text("${user.email}")
+                            ],
+                          )
+                        ],
                       ),
-                      const SizedBox(width: 16),
+                      Container(
+                          margin: const EdgeInsets.symmetric(vertical: 24),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 14),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(8)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.04),
+                                blurRadius: 12,
+                                offset: const Offset(
+                                    0, 4), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          height: 84,
+                          child: _merchantBanner(user)),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            "${user.displayName}",
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                          const Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 8),
+                              child: Text("AKUN")),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                                child: const Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text("Edit Profil",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.normal)),
+                                ),
+                                onPressed: () {}),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                                child: const Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text("Ganti Kata Sandi",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.normal)),
+                                ),
+                                onPressed: () {}),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                                child: const Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text("Ganti Nomor Ponsel",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.normal)),
+                                ),
+                                onPressed: () {}),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                              child: const Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text("Ganti Email",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.normal)),
+                              ),
+                              onPressed: () {},
                             ),
                           ),
-                          const SizedBox(height: 10),
-                          Text("${user.email}")
                         ],
+                      ),
+                      const Spacer(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 8),
+                              child: Text("PENGATURAN KAWASAN")),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                                child: const Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text("Ganti Kawasan",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.normal)),
+                                ),
+                                onPressed: () {
+                                 Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>const  PilihKwsn(),
+                                    ));
+                                }),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                                child: const Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text("Daftar Sub-Admin Kawasan",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.normal)),
+                                ),
+                                onPressed: () {}),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ReusableButton1(
+                          label: "KELUAR",
+                          onPressed: () async {
+                            await auth.signoutGoogle();
+                            // ignore: use_build_context_synchronously
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LoginPage(),
+                              ),
+                            );
+                            WidgetsFlutterBinding.ensureInitialized();
+                            var isRunning = await FlutterBackgroundService()
+                                .isServiceRunning();
+                            if (isRunning) {
+                              FlutterBackgroundService().sendData(
+                                {"action": "stopService"},
+                              );
+                            }
+                          },
+                          padding: const EdgeInsets.all(0),
+                          margin: const EdgeInsets.all(0),
+                        ),
                       )
                     ],
                   ),
-                  Container(
-                      margin: const EdgeInsets.symmetric(vertical: 24),
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 20, horizontal: 14),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(8)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.04),
-                            blurRadius: 12,
-                            offset: const Offset(
-                                0, 4), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      height: 84,
-                      child: _merchantBanner(user)),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                          child: Text("AKUN")),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: TextButton(
-                            child: const Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text("Edit Profil",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.normal)),
-                            ),
-                            onPressed: () {}),
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: TextButton(
-                            child: const Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text("Ganti Kata Sandi",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.normal)),
-                            ),
-                            onPressed: () {}),
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: TextButton(
-                            child: const Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text("Ganti Nomor Ponsel",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.normal)),
-                            ),
-                            onPressed: () {}),
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: TextButton(
-                          child: const Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text("Ganti Email",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.normal)),
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ReusableButton1(
-                      label: "KELUAR",
-                      onPressed: () async {
-                        await auth.signoutGoogle();
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginPage(),
-                          ),
-                        );
-                        WidgetsFlutterBinding.ensureInitialized();
-                        var isRunning =
-                            await FlutterBackgroundService().isServiceRunning();
-                        if (isRunning) {
-                          FlutterBackgroundService().sendData(
-                            {"action": "stopService"},
-                          );
-                        }
-                      },
-                      padding: const EdgeInsets.all(0),
-                      margin: const EdgeInsets.all(0),
-                    ),
-                  )
-                ],
+                ),
               ),
             ),
           ),
